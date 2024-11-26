ae5a64105cc1737533716376eece55dc37ab26d9
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2024-11-26 17:16:58 +0530

    feat(client): initialised app

---
+++ client/app/components/ChatComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChatComponent: React.FC = () => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+        <p className="">You can ask me anything!</p>
+        <input></input>
+    </div>
+  )
+}
+
+export default ChatComponent
